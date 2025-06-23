7d949d9cd36e5f5cf527e2d3c956989b7eb0d693
David Kruss <davidis1337@gmail.com>
2025-06-23 21:22:24 +1000

    Implement input validation, game loop

--- main.rb
+++ main.rb
@@ -1,76 +1,103 @@
-#TO DO:
-# - at the start of the game, create an array of four letters
-#   - each letter randomly chosen from ROYGBP
-# - there should be a Board class or object
-#   - this should have a string method displaying each guess and key combination on a new line
-# - there should be a hash which maps letters (ROYGBP) to their emojis (🔴🟠🟡🟢🔵🟣)
-
 COLOURS = "ROYGBP"
 PEGS = { R: "🔴", O: "🟠", Y: "🟡", G: "🟢", B: "🔵", P: "🟣"}
 KEYS = { correct: "⬛", correct_colour: "⬜", incorrect: ""}
+RULES = "Guess the secret code! 
 
-code = [COLOURS[rand(0..5)],COLOURS[rand(0..5)],COLOURS[rand(0..5)],COLOURS[rand(0..5)]]
-turns =  0
-string_rep = ""
-plays = []
+Four colour pegs - red, orange, yellow, green, blue, purple. 
+Type the first letter for each colour, e.g. ROYG 🔴🟠🟡🟢
 
-p code
+Black peg ⬛ - correct colour in correct space
+White peg ⬜ - correct colour in wrong space
 
-for peg in code do
-  string_rep += PEGS[peg.to_sym]
-end
+12 guesses
+Good luck!"
 
-puts string_rep
+def play_game
 
-while turns < 12
-  guess_string = ""
-  key_pegs = []
-  key_string = ""
-  code_remainder = []
-  guess_remainder = []
+  code = [COLOURS[rand(0..5)],COLOURS[rand(0..5)],COLOURS[rand(0..5)],COLOURS[rand(0..5)]]
+  turns =  0
+  plays = []
+  won = false
 
-  puts "What is your guess?"
-  guess = gets.chomp.split("")
-  p guess
+  puts RULES
 
-  for peg in guess do
-    guess_string += PEGS[peg.to_sym]
-  end
+  # For debugging only - shows 
 
-  puts guess_string
+  # string_rep = ""
+  # for peg in code do
+  #   string_rep += PEGS[peg.to_sym]
+  # end
+  # puts string_rep
 
-  code.each_with_index do |peg,idx|
-    if peg == guess[idx]
-      key_pegs << "correct"
-    else
-      code_remainder << peg
-      guess_remainder << guess[idx]
-    end 
-  end
+  while turns < 12
+    guess_string = ""
+    key_pegs = []
+    key_string = ""
+    code_remainder = []
+    guess_remainder = []
+    current_guess = []
+    valid_guess = false
 
-  for colour in COLOURS.split("") do
-    right_colour_wrong_space = [guess_remainder.count(colour),code_remainder.count(colour)].min
-    for peg in (1..right_colour_wrong_space)
-      key_pegs << "correct_colour"
+    while !valid_guess
+      puts "What is your guess?"
+      current_guess = gets.chomp.upcase.split("")
+      if current_guess.length != 4
+        puts "Incorrect length. Four colours required."
+      elsif !(current_guess - COLOURS.split("")).empty?
+        puts "Invalid input. Type the first name of the colour: R, O, Y, G, B, P."
+      else
+        valid_guess = true
+      end
     end
-  end
 
-  key_pegs.sort!
-  #p key_pegs
+    for peg in current_guess do
+      guess_string += PEGS[peg.to_sym]
+    end
 
-  for key in key_pegs do
-    key_string += KEYS[key.to_sym]
-  end
+    code.each_with_index do |peg,idx|
+      if peg == current_guess[idx]
+        key_pegs << "correct"
+      else
+        code_remainder << peg
+        guess_remainder << current_guess[idx]
+      end 
+    end
 
-  puts key_string
+    for colour in COLOURS.split("") do
+      right_colour_wrong_space = [guess_remainder.count(colour),code_remainder.count(colour)].min
+      for peg in (1..right_colour_wrong_space)
+        key_pegs << "correct_colour"
+      end
+    end
 
-  turns += 1
+    # Ensures no additional information can be gained from the order of the black and white pegs
+    key_pegs.sort!
 
-  plays << turns.to_s + "\t" + guess_string + "\t" + key_string
+    for key in key_pegs do
+      key_string += KEYS[key.to_sym]
+    end
 
-  puts "\t⚫⚫⚫⚫"
-  for play in plays
-    puts play
+    turns += 1
+
+    plays << turns.to_s + "\t" + guess_string + "\t" + key_string
+
+    puts "\t⚫⚫⚫⚫"
+    for play in plays
+      puts play
+    end
+
+    if key_pegs == ["correct","correct","correct","correct"]
+      puts "Congratulations! You won!"
+      won = true
+      break
+    end
+
   end
   
+  if !won
+    puts "You lost - better luck next time!"
+  end
+
 end
+
+play_game
